6762452fcee4c4d0ec1a738b4f5e5d2d4671b3b4
Adubbz <Adubbz@users.noreply.github.com>
2023-04-22 12:57:36 +1000

    Added asm-differ

---
+++ diff_settings.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def apply(config, args):
+    config["arch"] = 'mipsel'
+    config['baseimg'] = 'rom/SCUS_971.11'
+    config['myimg'] = 'build/SCUS_971.11.prelim.elf'
+    config['mapfile'] = 'build/SCUS_971.11.prelim.map'
+    config['objdump_executable'] = 'tools/mips64r5900el-ps2-elf-objdump'
+    config['source_directories'] = ['src', 'include', 'asm']
