4fb4feb456698f7ddbb8d601fb55273f23d6b061
Adubbz <Adubbz@users.noreply.github.com>
2023-04-26 19:05:31 +1000

    Missed a .exe extension

--- diff_settings.py
+++ diff_settings.py
@@ -5,5 +5,5 @@ def apply(config, args):
     config['baseimg'] = 'rom/SCUS_971.11'
     config['myimg'] = 'build/SCUS_971.11.prelim.elf'
     config['mapfile'] = 'build/SCUS_971.11.prelim.map'
-    config['objdump_executable'] = 'tools/mips64r5900el-ps2-elf-objdump'
+    config['objdump_executable'] = 'tools/mips64r5900el-ps2-elf-objdump.exe'
     config['source_directories'] = ['src', 'include', 'asm']
