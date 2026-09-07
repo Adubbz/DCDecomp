32ab12d4110d52df2bf25dffbeb09daff4f9ac9b
Dylan <dylan@local>
2026-09-07 21:47:36 +0000

    Match closeGiftag
    m2c draft compiled as-is
    
    100% match; byte-perfect in the linked image.
    
    Written through scripts/decomp/worker.py (m2c draft compiled as-is, no model call).

--- src/dispctrl.cpp
+++ src/dispctrl.cpp
@@ -11,7 +11,10 @@ INCLUDE_RODATA("asm/nonmatchings/dispctrl", @230);
 
 INCLUDE_ASM("asm/nonmatchings/dispctrl", Sizeof__6CBoundFv);
 INCLUDE_ASM("asm/nonmatchings/dispctrl", openGiftag__FP13sceVif1Packet);
-INCLUDE_ASM("asm/nonmatchings/dispctrl", closeGiftag__FP13sceVif1Packet);
+void closeGiftag(sceVif1Packet *arg0) {
+    sceVif1PkCloseGifTag(arg0);
+    sceVif1PkCloseDirectCode(arg0);
+}
 INCLUDE_ASM("asm/nonmatchings/dispctrl", Draw__10CDebugFontFv);
 INCLUDE_ASM("asm/nonmatchings/dispctrl", SearchPTS__FPUiPc);
 INCLUDE_ASM("asm/nonmatchings/dispctrl", SearchPTS__FPUii);
