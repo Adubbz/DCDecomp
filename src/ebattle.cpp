37038a5de628059021184cac8462a3b6e8449c5a
Dylan <dylan@local>
2026-09-07 21:26:34 +0000

    Match CommandTEX_ANIME_END
    m2c draft compiled as-is
    
    100% match; byte-perfect in the linked image.
    
    Written through scripts/decomp/worker.py (m2c draft compiled as-is, no model call).

--- src/ebattle.cpp
+++ src/ebattle.cpp
@@ -12,7 +12,8 @@ INCLUDE_ASM("asm/nonmatchings/ebattle", CommandTEX_ANIME__FPPv);
 INCLUDE_ASM("asm/nonmatchings/ebattle", CommandTEX_ANIME_DATA__FPPv);
 INCLUDE_ASM("asm/nonmatchings/ebattle", CommandTEX_ANIME_DATA2__FPPv);
 INCLUDE_ASM("asm/nonmatchings/ebattle", CommandTEX_SCROLL_DATA__FPPv);
-INCLUDE_ASM("asm/nonmatchings/ebattle", CommandTEX_ANIME_END__FPPv);
+void CommandTEX_ANIME_END(void **arg0) {
+}
 
 /* External */
 
