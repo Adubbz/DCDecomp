6018cc10b6da07b3151e3ff31b3232d9d9880c5a
carrdylan52 <carrdylan52@gmail.com>
2026-09-13 16:58:52 +0000

    Merge gamemode suffix into main

---
+++ asm/nonmatchings/main/__ct__11MotionParamFv.s
@@ -0,0 +1,10 @@
+.include "macro.inc"
+
+.set noat
+.set noreorder
+
+glabel __ct__11MotionParamFv
+    /* 043710 00143610 28168070 */  paddub     $2, $4, $0
+    /* 043714 00143614 0800E003 */  jr         $31
+    /* 043718 00143618 00000000 */   nop
+    /* 04371C 0014361C 00000000 */  nop
