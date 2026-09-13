2a5b64d372a1638282dbb2496aec053b2dda024f
carrdylan52 <carrdylan52@gmail.com>
2026-09-13 16:31:14 +0000

    Match CDungeonEventData::CheckSwitch
    Return whether the event slot has a record and both runtime switches are set.

--- src/dungeoneventdata.cpp
+++ src/dungeoneventdata.cpp
@@ -2,7 +2,16 @@
 
 INCLUDE_ASM("asm/nonmatchings/dungeoneventdata", Set__17CDungeonEventDataFP13CDungeonEvent);
 INCLUDE_RODATA("asm/nonmatchings/dungeoneventdata", @3309);
-INCLUDE_ASM("asm/nonmatchings/dungeoneventdata", CheckSwitch__17CDungeonEventDataFv);
+
+int CDungeonEventData::CheckSwitch(void) {
+    if (event == NULL) {
+        return 0;
+    }
+    if (unk_38 != 0 && unk_34 != 0) {
+        return 1;
+    }
+    return 0;
+}
 
 void CDungeonEventData::Stop(void) {
     unk_34 = 0;
