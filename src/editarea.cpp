7e3a71931e44e9c42ccbf7fc805695140ad45189
Dylan <dylan@local>
2026-09-13 01:14:46 +0000

    Match CEditArea::SetPartsExtra

--- src/editarea.cpp
+++ src/editarea.cpp
@@ -29,7 +29,15 @@ void CEditArea::SetPartsID(int x, int y, int parts_id_) {
     }
     grid[x][y].parts_id = parts_id_;
 }
-INCLUDE_ASM("asm/nonmatchings/editarea", SetPartsExtra__9CEditAreaFiii);
+void CEditArea::SetPartsExtra(int x, int y, int parts_extra_) {
+    if (x < 0 || x >= width) {
+        return;
+    }
+    if (y < 0 || y >= height) {
+        return;
+    }
+    grid[x][y].parts_extra = parts_extra_;
+}
 INCLUDE_ASM("asm/nonmatchings/editarea", SetCode__9CEditAreaFiii);
 INCLUDE_ASM("asm/nonmatchings/editarea", GetPartsID__9CEditAreaFii);
 INCLUDE_ASM("asm/nonmatchings/editarea", GetCode__9CEditAreaFii);
