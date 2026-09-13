96a033e386b242e2887d1c799c69dee755d2cec4
carrdylan52 <carrdylan52@gmail.com>
2026-09-13 18:22:37 +0000

    Document nowload interface

--- include/nowload.hpp
+++ include/nowload.hpp
@@ -8,12 +8,38 @@
  */
 
 /**
+ * Reports whether the loading-screen operation has ended.
+ *
+ * @mangled check_now_loading__Fv
+ * @address 0x153F40
+ * @size 0xC
+ */
+int check_now_loading(void);
+
+/**
+ * Clears the flag set after the loading screen completes a field.
+ *
+ * @mangled clear_now_loading_vsync_end__Fv
+ * @address 0x153F50
+ * @size 0xC
+ */
+void clear_now_loading_vsync_end(void);
+
+/**
+ * Reports whether the loading screen has completed a field.
+ *
+ * @mangled check_now_loading_vsync_end__Fv
+ * @address 0x153F60
+ * @size 0xC
+ */
+int check_now_loading_vsync_end(void);
+
+/**
  * Waits for the loading screen to reach the end of a field.
  *
  * @mangled wait_now_loading_vsync__Fv
  * @address 0x153F70
  * @size 0x3C
- * @unknownret
  */
 void wait_now_loading_vsync(void);
 
@@ -24,4 +50,4 @@ void wait_now_loading_vsync(void);
  * @address 0x153FB0
  * @size 0x10
  */
-void now_loading_off();
+void now_loading_off(void);
