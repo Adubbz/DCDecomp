4b57891945e2d1301633a8825a9cbcd31861fe8e
carrdylan52 <carrdylan52@gmail.com>
2026-09-13 16:30:39 +0000

    Match CDungeonEventData::Stop
    Disable the event slot by clearing its runtime switch.

--- src/dungeoneventdata.cpp
+++ src/dungeoneventdata.cpp
@@ -3,7 +3,10 @@
 INCLUDE_ASM("asm/nonmatchings/dungeoneventdata", Set__17CDungeonEventDataFP13CDungeonEvent);
 INCLUDE_RODATA("asm/nonmatchings/dungeoneventdata", @3309);
 INCLUDE_ASM("asm/nonmatchings/dungeoneventdata", CheckSwitch__17CDungeonEventDataFv);
-INCLUDE_ASM("asm/nonmatchings/dungeoneventdata", Stop__17CDungeonEventDataFv);
+
+void CDungeonEventData::Stop(void) {
+    unk_34 = 0;
+}
 
 void CDungeonEventData::Start(void) {
     unk_34 = 1;
