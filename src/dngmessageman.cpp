c3b14f1a9a0ab5cb8869735751803ebca1c47e31
carrdylan52 <carrdylan52@gmail.com>
2026-09-13 17:15:00 +0000

    Match CDngMessageMan::LimmitZone
    Select the warning for the active dungeon restriction zone at 0x1B5B90.

--- src/dngmessageman.cpp
+++ src/dngmessageman.cpp
@@ -3,6 +3,21 @@
 #include "dun/gameloop.hpp"
 #include "userstatus.hpp"
 
-INCLUDE_ASM("asm/nonmatchings/dngmessageman", LimmitZone__14CDngMessageManFv);
+void CDngMessageMan::LimmitZone(void) {
+    int zone = UserStatus->res_limit_zone_current;
+
+    if (zone >= 0 && zone < 6) {
+        unk_24 = 0xB;
+        unk_0C = UserStatus->cur_chara + 0x32;
+    }
+    if (UserStatus->res_limit_zone_current == 0xA) {
+        unk_24 = 0xD;
+    }
+    if (UserStatus->res_limit_zone_current == 0xB) {
+        unk_24 = 0xE;
+    }
+    unk_04 = 0xF0;
+    unk_1C = 0;
+}
 INCLUDE_ASM("asm/nonmatchings/dngmessageman", SetStatus_Dry__14CDngMessageManFfff);
 INCLUDE_ASM("asm/nonmatchings/dngmessageman", SetSteevMes__14CDngMessageManFi);
