01a18b8fccd5e3f908384bfa7848ea32447aa4d4
carrdylan52 <carrdylan52@gmail.com>
2026-09-13 12:35:24 +0000

    Match BtSetMapJumpFloor
    Store the requested dungeon floor for the next battle-map jump.
    
    Ported from Dayuppy's decompilation.

--- src/btsysscript.cpp
+++ src/btsysscript.cpp
@@ -15,7 +15,9 @@ INCLUDE_ASM("asm/nonmatchings/btsysscript", BtSystemScriptInit__Fv);
 INCLUDE_ASM("asm/nonmatchings/btsysscript", BtSystemScriptAfter__Fv);
 INCLUDE_ASM("asm/nonmatchings/btsysscript", BtSystemScriptRun__FiP14CDataAlloc2_1_);
 
-INCLUDE_ASM("asm/nonmatchings/btsysscript", BtSetMapJumpFloor__Fi);
+void BtSetMapJumpFloor(int floor) {
+    BtMapJumpFloor = floor;
+}
 
 INCLUDE_ASM("asm/nonmatchings/btsysscript", GetStackInt__FP12RS_STACKDATA__2);
 INCLUDE_ASM("asm/nonmatchings/btsysscript", GetStackFloat__FP12RS_STACKDATA__2);
