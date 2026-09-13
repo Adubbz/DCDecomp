4cbcac9173da14167a646308864c412f5d66c36c
carrdylan52 <carrdylan52@gmail.com>
2026-09-13 18:48:08 +0000

    Match BtGetAttach_Init
    Ported from the matching decompilation by Dayuppy1707723.

--- include/btitem.hpp
+++ include/btitem.hpp
@@ -8,3 +8,12 @@
  * @size 0x4C
  */
 void LoadActiveItemIcon(void);
+
+/**
+ * Gives the player an attachment and opens its acquisition message.
+ *
+ * @mangled BtGetAttach_Init__Fii
+ * @address 0x1D3A70
+ * @size 0x90
+ */
+void BtGetAttach_Init(int dungeon, int item_no);
