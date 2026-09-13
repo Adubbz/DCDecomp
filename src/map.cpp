90c4b911e86901ae8498c1d92b10c4a718a811cf
carrdylan52 <carrdylan52@gmail.com>
2026-09-13 16:36:04 +0000

    Match CMap::GetObject
    Return the requested map object when its slot index is in range.

--- src/map.cpp
+++ src/map.cpp
@@ -2,6 +2,13 @@
 
 INCLUDE_ASM("asm/nonmatchings/map", SetObject__4CMapFiP9CFrameVu1ii);
 INCLUDE_ASM("asm/nonmatchings/map", SetObject__4CMapFP9CFrameVu1ii);
-INCLUDE_ASM("asm/nonmatchings/map", GetObject__4CMapFi);
+
+CMapObject *CMap::GetObject(int index) {
+    if (index >= 10 || index < 0) {
+        return NULL;
+    }
+    return &object[index];
+}
+
 INCLUDE_ASM("asm/nonmatchings/map", Draw__4CMapFv);
 INCLUDE_ASM("asm/nonmatchings/map", Initialize__4CMapFv);
