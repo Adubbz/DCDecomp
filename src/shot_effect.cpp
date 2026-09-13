58c759e37af7a85b4cc4bd1b0fb82a31d5953fb9
Dylan <dylan@local>
2026-09-13 01:07:55 +0000

    Match CSHOT_EFFECT::SetLifeTime
    Define CSHOT_EFFECT::SetLifeTime in src/shot_effect.cpp; its reference
    assembly moves to asm/matchings/shot_effect/.

--- src/shot_effect.cpp
+++ src/shot_effect.cpp
@@ -23,7 +23,14 @@ INCLUDE_RODATA("asm/nonmatchings/shot_effect", @977__2);
 INCLUDE_ASM("asm/nonmatchings/shot_effect", SetWait__12CSHOT_EFFECTFi);
 INCLUDE_ASM("asm/nonmatchings/shot_effect", SetNoSound__12CSHOT_EFFECTFv);
 INCLUDE_ASM("asm/nonmatchings/shot_effect", SetRandomRate__12CSHOT_EFFECTFf);
-INCLUDE_ASM("asm/nonmatchings/shot_effect", SetLifeTime__12CSHOT_EFFECTFi);
+void CSHOT_EFFECT::SetLifeTime(s32 arg0) {
+    s32 temp_6;
+
+    temp_6 = this->unk_A150;
+    if (temp_6 != -1) {
+        this->unk_A0F0[temp_6] = arg0;
+    }
+}
 void CSHOT_EFFECT::SetEnemyAttr(s32 arg0) {
     s32 temp_6;
 
