0266e36a602c61a6eb69a67bbf3fd0d141e408d8
Dylan <dylan@local>
2026-09-13 01:07:46 +0000

    Match CSHOT_EFFECT::SetEnemyAttr
    Define CSHOT_EFFECT::SetEnemyAttr in src/shot_effect.cpp; its reference
    assembly moves to asm/matchings/shot_effect/.

--- src/shot_effect.cpp
+++ src/shot_effect.cpp
@@ -24,7 +24,14 @@ INCLUDE_ASM("asm/nonmatchings/shot_effect", SetWait__12CSHOT_EFFECTFi);
 INCLUDE_ASM("asm/nonmatchings/shot_effect", SetNoSound__12CSHOT_EFFECTFv);
 INCLUDE_ASM("asm/nonmatchings/shot_effect", SetRandomRate__12CSHOT_EFFECTFf);
 INCLUDE_ASM("asm/nonmatchings/shot_effect", SetLifeTime__12CSHOT_EFFECTFi);
-INCLUDE_ASM("asm/nonmatchings/shot_effect", SetEnemyAttr__12CSHOT_EFFECTFi);
+void CSHOT_EFFECT::SetEnemyAttr(s32 arg0) {
+    s32 temp_6;
+
+    temp_6 = this->unk_A150;
+    if (temp_6 != -1) {
+        this->unk_A110[temp_6] = arg0;
+    }
+}
 void CSHOT_EFFECT::SetDmg(s32 arg0) {
     s32 temp_6;
 
